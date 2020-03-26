d2d9d7f45f7644e42a432a6ba7459db4f056fe00
Joel Kelly <joel.kelly.philly@gmail.com>
2020-03-26 14:33:07 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,6 @@
 # Build an array of numbers to test if the number is divisible by any of them
 def prime?(number)
-  if number < 0 || number == -1 || number == 1
+  if number <= 0 || number == 1
     return false
   else
     my_array = (2..(number)).to_a
